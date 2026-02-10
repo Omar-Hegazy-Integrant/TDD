12701d16d5396adbdd6df7ed07e36db94ef9cf62
Omar-Hegazy-Integrant <oh1021@dexcom.com>
2026-02-10 19:58:40 +0200

    Initial commit

---
+++ TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by Omar Hegazy on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDDTests/TDDTests.swift
@@ -0,0 +1,32 @@
+//
+//  TDDTests.swift
+//  TDDTests
+//
+//  Created by Omar Hegazy on 10/02/2026.
+//
+
+import Testing
+
+struct TDDTests {
+
+    @Test func `test multiplication`() async throws {
+        let five = Dollar(5)
+        five.times(2)
+        #expect(five.amount == 10)
+    }
+    
+    
+
+}
+
+class Dollar {
+    private(set) var amount: Int
+    
+    init(_ amount: Int) {
+        self.amount = 0
+    }
+    
+    func times(_ multiplier: Int) {
+        amount = 10
+    }
+}
