69521e9a4a32cd0ef1c42a5948a8588298f75ed3
Omar-Hegazy-Integrant <oh1021@dexcom.com>
2026-02-12 11:33:27 +0200

    remove equatable conformance

--- TDD/TDD/Dollar.swift
+++ TDD/TDD/Dollar.swift
@@ -6,7 +6,7 @@
 //
 
 
-final class Dollar: Equatable {
+final class Dollar {
     let amount: Int
     
     init(_ amount: Int) {
@@ -15,9 +15,5 @@ final class Dollar: Equatable {
     
     func times(_ multiplier: Int) -> Dollar {
         Dollar(amount * multiplier)
-    }
-
-    static func == (lhs: Dollar, rhs: Dollar) -> Bool {
-        lhs.amount == rhs.amount
     }
 }
