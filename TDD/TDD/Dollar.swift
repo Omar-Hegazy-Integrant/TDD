355d3246f8e0d35df0be6f16db243ff68b38249f
Omar-Hegazy-Integrant <oh1021@dexcom.com>
2026-02-15 11:28:41 +0200

    remove duplication by introducing Money class

--- TDD/TDD/Dollar.swift
+++ TDD/TDD/Dollar.swift
@@ -5,36 +5,27 @@
 //  Created by Omar Hegazy on 12/02/2026.
 //
 
-
-final class Dollar: Equatable {
-    private let amount: Int
+class Money: Equatable {
+    let amount: Int
     
     init(_ amount: Int) {
         self.amount = amount
     }
     
-    func times(_ multiplier: Int) -> Dollar {
-        Dollar(amount * multiplier)
-    }
-
-    static func == (lhs: Dollar, rhs: Dollar) -> Bool {
+    static func == (lhs: Money, rhs: Money) -> Bool {
         lhs.amount == rhs.amount
     }
 }
 
-
-final class Franc: Equatable {
-    private let amount: Int
-    
-    init(_ amount: Int) {
-        self.amount = amount
+final class Dollar: Money {
+    func times(_ multiplier: Int) -> Dollar {
+        Dollar(amount * multiplier)
     }
-    
+}
+
+
+final class Franc: Money {
     func times(_ multiplier: Int) -> Franc {
         Franc(amount * multiplier)
-    }
-
-    static func == (lhs: Franc, rhs: Franc) -> Bool {
-        lhs.amount == rhs.amount
     }
 }
